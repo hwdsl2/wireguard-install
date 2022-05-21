7519c48b3f5d61767214a43d95e8ac1e15c39fa0
hwdsl2 <hwdsl2@users.noreply.github.com>
2022-05-21 13:08:37 -0500

    Cleanup

--- wireguard-install.sh
+++ wireguard-install.sh
@@ -212,7 +212,7 @@ if [[ ! -e /etc/wireguard/wg0.conf ]]; then
 		apt-get install -y wget
 	fi
 	echo
-	echo 'Welcome to this WireGuard road warrior installer!'
+	echo 'Welcome to this WireGuard VPN server installer!'
 	# If system has a single IPv4, it is selected automatically. Else, ask the user
 	if [[ $(ip -4 addr | grep inet | grep -vEc '127(\.[0-9]{1,3}){3}') -eq 1 ]]; then
 		ip=$(ip -4 addr | grep inet | grep -vE '127(\.[0-9]{1,3}){3}' | cut -d '/' -f 1 | grep -oE '[0-9]{1,3}(\.[0-9]{1,3}){3}')
